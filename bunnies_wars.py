7aef1b989ff1d62047c4df762380d9148f49c2c7
hearthdesign <hearthxdesign@gmail.com>
2025-07-28 12:18:01 +0200

    add import random for pc choice add steps for IPO flow

---
+++ bunnies_wars.py
@@ -0,0 +1,12 @@
+import random
+
+# welcome message, input player name.
+# create 2 field for user and computer with a given size and number of carrots.
+# Class Field (self, size, num_carrots).
+# place carrots user manually and pc randomly.
+# begin game loop 
+# dig to collect carrots in turns (player 1, PC)
+# check results,
+#  track scores 
+# print fields
+# end the game when all carrots are collected
